02b4acafd5697d178b98e3b3d4696059b56023dd
maybemaby <74226064+maybemaby@users.noreply.github.com>
2026-01-03 11:14:55 -0800

    fix: client doesn't work with alternative port

--- frontend/src/lib/api/client.ts
+++ frontend/src/lib/api/client.ts
@@ -2,7 +2,7 @@ import createClient from 'openapi-fetch';
 import type { paths } from './spec';
 
 export const apiClient = createClient<paths>({
-	baseUrl: 'http://localhost:8000',
+	baseUrl: import.meta.env.DEV ? 'http://localhost:8000' : window.location.origin,
 	credentials: 'include',
 	mode: 'cors'
 });
